d8563e0f3afe3a32851715507bff20dc36e6d7a2
Patrick Ng <png@hawaii.edu>
2023-03-19 16:09:54 -1000

    trying to get clear_routine() to work

--- homework/homework/problem3.py
+++ homework/homework/problem3.py
@@ -3,6 +3,7 @@
 import rclpy
 from rclpy.node import Node
 from turtlesim.srv import Spawn, SetPen, TeleportAbsolute
+from std_srvs.srv import Empty
 
 
 class ServiceHw(Node):
@@ -10,11 +11,12 @@ class ServiceHw(Node):
     def __init__(self):
         super().__init__('problem_3') # super() calls the Node parent class
 
-        # # Create a Spawn client for default turtlesim services
-        # self.spawn = self.create_client(Spawn, 'Spawn')
-        # while not self.spawn.wait_for_service(timeout_sec=1.0):
-        #     self.get_logger().info('service not available, waiting again...')
-        # self.spawn_req = Spawn.Request()
+        # Create Clear client for default turtlesim services
+        self.clear = self.create_client(Empty, '/clear')
+        while not self.clear.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('CLEAR_DEBUG:service not available, waiting again...')
+        self.clear_req = Empty.Request()
+
 
         # Create SetPen client for default turtlesim services
         self.setpen = self.create_client(SetPen, '/turtle1/set_pen') # srv_name is the existing service (server)
@@ -29,14 +31,11 @@ class ServiceHw(Node):
         self.teleport_req = TeleportAbsolute.Request()
 
     def send_request(self):
-        # Spawn service parameters
-        # self.spawn_req.x = 20.0
-        # self.spawn_req.y = 10.0
-        # self.spawn_req.theta = 0.0
-        # self.spawn_req.name = 'turtle1'
-        # self.future = self.cli.call_async(self.spawn_req)
-        # rclpy.spin_until_future_complete(self, self.future)
-
+        def clear_routine():
+            self.clear_req
+            self.future = self.clear.call_async(self.clear_req)
+            rclpy.spin_until_future_complete(self, self.future)
+            
         # SetPen service parameters
         self.setpen_req.r = 0
         self.setpen_req.g = 0
@@ -45,42 +44,23 @@ class ServiceHw(Node):
         self.setpen_req.off = 0
         self.future = self.setpen.call_async(self.setpen_req)
         rclpy.spin_until_future_complete(self, self.future)
+        clear_routine()
 
-        # TeleportAbsolute_1 service parameters 
-        self.teleport_req.x = 20.0
-        self.teleport_req.y = 15.0
-        self.teleport_req.theta = 0.0
-        self.future = self.teleport.call_async(self.teleport_req)
-        rclpy.spin_until_future_complete(self, self.future)
-
-        # TeleportAbsolute_2 service parameters 
-        self.teleport_req.x = 20.0
-        self.teleport_req.y = 5.0
-        self.teleport_req.theta = 0.0
-        self.future = self.teleport.call_async(self.teleport_req)
-        rclpy.spin_until_future_complete(self, self.future)
-
-        # TeleportAbsolute_3 service parameters 
-        self.teleport_req.x = 20.0
-        self.teleport_req.y = 10.0
-        self.teleport_req.theta = 0.0
-        self.future = self.teleport.call_async(self.teleport_req)
-        rclpy.spin_until_future_complete(self, self.future)
-
-        # TeleportAbsolute_4 service parameters 
-        self.teleport_req.x = 15.0
-        self.teleport_req.y = 10.0
-        self.teleport_req.theta = 0.0
-        self.future = self.teleport.call_async(self.teleport_req)
-        rclpy.spin_until_future_complete(self, self.future)
-
-        # TeleportAbsolute_5 service parameters 
-        self.teleport_req.x = 25.0
-        self.teleport_req.y = 10.0
-        self.teleport_req.theta = 0.0
-        self.future = self.teleport.call_async(self.teleport_req)
-        rclpy.spin_until_future_complete(self, self.future)
-
+        coordinates = [(20.0, 10.0),
+                        (20.0, 15.0),
+                        (20.0, 5.0),
+                        (20.0, 10.0),
+                        (15.0, 10.0),
+                        (25.0, 10.0)]
+        for i, (x, y) in enumerate(coordinates):
+            self.teleport_req.x = x
+            self.teleport_req.y = y
+            self.teleport_req.theta = 0.0
+            if i == 0:
+                self.get_logger().info('i=1')
+                clear_routine()
+            self.future = self.teleport.call_async(self.teleport_req)
+            rclpy.spin_until_future_complete(self, self.future)    
 
 def main(args=None):
     rclpy.init()
