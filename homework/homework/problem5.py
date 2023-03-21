71970924cd2c98806fa33337c6c1bb5dfe46a8a0
Patrick Ng <png@hawaii.edu>
2023-03-20 20:46:04 -1000

    tf stable

--- homework/homework/problem5.py
+++ homework/homework/problem5.py
@@ -41,49 +41,85 @@ class FramePublisher(Node):
     def __init__(self):
         super().__init__('turtle_tf2_frame_publisher')
 
-        # Declare and acquire `turtlename` parameter
-        self.turtlename = self.declare_parameter(
-          'turtlename', 'turtle').get_parameter_value().string_value
+        # Declare and acquire `boatname` parameter
+        self.boatname = self.declare_parameter(
+          'boatname', 'usv_').get_parameter_value().string_value
+
+        # Declare and acquire `obstaclename` parameter
+        self.obstaclename = self.declare_parameter(
+          'obstaclename', 'obstacle_').get_parameter_value().string_value
+        
+        self.observername = self.declare_parameter(
+          'observername', 'observer_').get_parameter_value().string_value
+
 
         # Initialize the transform broadcaster
         self.tf_broadcaster = TransformBroadcaster(self)
 
-        # Subscribe to a turtle{1}{2}/pose topic and call handle_turtle_pose
+
+        # Subscribe to a turtle{1}{2}/pose topic and call handle pose functions
         # callback function on each message
-        self.subscription = self.create_subscription(
+        self.subscription_1 = self.create_subscription(
             Pose,
-            f'/Patrick_turtlesim/{self.turtlename}/pose',
-            self.handle_turtle_pose,
+            f'/Patrick_turtlesim/{self.boatname}/pose',
+            self.handle_boat_pose,
             1)
-        self.subscription  # prevent unused variable warning
+        self.subscription_1  # prevent unused variable warning
 
-    def handle_turtle_pose(self, msg):
+        self.subscription_2 = self.create_subscription(
+            Pose,
+            f'/Patrick_turtlesim/{self.obstaclename}/pose',
+            self.handle_obstacle_pose,
+            1)
+        self.subscription_2  # prevent unused variable warning
+
+        self.subscription_3 = self.create_subscription(
+            Pose,
+            f'/Patrick_turtlesim/{self.observername}/pose',
+            self.handle_observer_pose,
+            1)
+        self.subscription_3  # prevent unused variable warning
+
+        # create a subscription to multiple Pose topics
+
+    def handle_boat_pose(self, msg):
+        t_world = self.transform_constructor('world', 'ENU', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+        self.tf_broadcaster.sendTransform(t_world)
+        t_coord = self.transform_constructor('ENU', 'NED', 20.0, 10.0, 0.0, np.pi/2, 0.0, np.pi)
+        self.tf_broadcaster.sendTransform(t_coord)
+        t_boat = self.transform_constructor('ENU', self.boatname, msg.x, msg.y, 0.0, msg.theta, 0.0, 0.0)
+        self.tf_broadcaster.sendTransform(t_boat)
+    
+    def handle_obstacle_pose(self, msg):
+        t_world = self.transform_constructor('world', 'ENU', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+        self.tf_broadcaster.sendTransform(t_world)
+        t_coord = self.transform_constructor('ENU', 'NED', 20.0, 10.0, 0.0, np.pi/2, 0.0, np.pi)
+        self.tf_broadcaster.sendTransform(t_coord)
+        t_obstacle = self.transform_constructor('ENU', self.obstaclename, msg.x, msg.y, 0.0, msg.theta, 0.0, 0.0)
+        self.tf_broadcaster.sendTransform(t_obstacle)
+    
+    def handle_observer_pose(self, msg):
+        t_world = self.transform_constructor('world', 'ENU', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+        self.tf_broadcaster.sendTransform(t_world)
+        t_coord = self.transform_constructor('ENU', 'NED', 20.0, 10.0, 0.0, np.pi/2, 0.0, np.pi)
+        self.tf_broadcaster.sendTransform(t_coord)
+        t_observer = self.transform_constructor('ENU', self.observername, msg.x, msg.y, 0.0, msg.theta, 0.0, 0.0)
+        self.tf_broadcaster.sendTransform(t_observer)
+
+    def transform_constructor(self, parent_frame, child_frame, x, y, z, yaw, pitch, roll):
         t = TransformStamped()
-
-        # Read message content and assign it to
-        # corresponding tf variables
         t.header.stamp = self.get_clock().now().to_msg()
-        t.header.frame_id = 'world'
-        t.child_frame_id = self.turtlename
-
-        # Turtle only exists in 2D, thus we get x and y translation
-        # coordinates from the message and set the z coordinate to 0
-        t.transform.translation.x = msg.x
-        t.transform.translation.y = msg.y
-        t.transform.translation.z = 0.0
-
-        # For the same reason, turtle can only rotate around one axis
-        # and this why we set rotation in x and y to 0 and obtain
-        # rotation in z axis from the message
-        q = quaternion_from_euler(0, 0, msg.theta)
+        t.header.frame_id = parent_frame
+        t.child_frame_id = child_frame
+        t.transform.translation.x = x
+        t.transform.translation.y = y
+        t.transform.translation.z = z
+        q = quaternion_from_euler(roll, pitch, yaw)
         t.transform.rotation.x = q[0]
         t.transform.rotation.y = q[1]
         t.transform.rotation.z = q[2]
         t.transform.rotation.w = q[3]
-
-        # Send the transformation
-        self.tf_broadcaster.sendTransform(t)
-
+        return t
 
 def main():
     rclpy.init()
